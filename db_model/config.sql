6aa390aeaffeafb069b50eddbbc545383b896027
kgmuzungu <klaus.glanzer@gmx.at>
2017-11-03 18:39:53 +0100

    new DB insert data

---
+++ db_model/config.sql
@@ -0,0 +1,15 @@
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`idconfig`, `conf_iddevice`, `conf_type`, `conf_name`, `conf_beschreibung`, `conf_prechecked`, `conf_preis`, `conf_image_path`, `conf_select_option`) VALUES
+(1, 1, 'Zubehoer', 'Monitor', 'Samsung 24\"', 1, '10.00', NULL, NULL),
+(2, 1, 'Zubehoer', 'Dockingstation', 'super station', 0, '5.00', NULL, NULL),
+(3, 1, 'Software', 'Office 365', 'Business in the cloud', 1, '5.00', NULL, NULL),
+(4, 1, 'Software', 'Kaspersky', 'Antivirus', 1, '5.00', NULL, NULL),
+(5, 2, 'Zubehoer', 'Monitor', 'Samsung 22\"', 1, '5.00', NULL, NULL),
+(6, 2, 'Software', 'Office 2017', 'not in the cloud', 0, '5.00', NULL, NULL),
+(7, 3, 'Zubehoer', 'Monitor', 'Samsung 24\"', 1, '10.00', NULL, NULL),
+(8, 3, 'Software', 'Office 365', 'Business in the cloud', 1, '5.00', NULL, NULL);
+
+
