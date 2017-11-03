8dd4003b9192b62466a39713a8964508be1ae077
kgmuzungu <klaus.glanzer@gmx.at>
2017-11-03 18:41:41 +0100

    cleaning up

--- db_model/insert_devices.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: 
--- Date: 2017-10-22 18:54
-*/
-INSERT INTO `devices` (`iddevices`,`dev_arbeitsplatz`,`dev_device`,`dev_beschreibung`,`dev_preis`,`dev_anwendung`,`dev_geeignet`,`dev_imagepath`) VALUES (1,'Business','HP ProBook 555','Beschreibung blabla',25.00,'Beschreibung blabla','geeignet',NULL);
-INSERT INTO `devices` (`iddevices`,`dev_arbeitsplatz`,`dev_device`,`dev_beschreibung`,`dev_preis`,`dev_anwendung`,`dev_geeignet`,`dev_imagepath`) VALUES (2,'Premium','HP ProBook 777','2Beschreibung blabla2',30.00,'2Beschreibung blabla2','geeignet2',NULL);
-INSERT INTO `devices` (`iddevices`,`dev_arbeitsplatz`,`dev_device`,`dev_beschreibung`,`dev_preis`,`dev_anwendung`,`dev_geeignet`,`dev_imagepath`) VALUES (3,'Mobil','HP ProBook mob','3Beschreibung blabla3',32.00,'3Beschreibung blabla3','geeignet3',NULL);
